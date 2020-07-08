aa47b201521a16484a122f54213ce2d6b1f4d4c1
Amir <hesamian@uwm.edu>
2020-07-07 21:32:17 -0500

    Added missing interface and create an executable upon build

--- Deployr.Web.Contracts/ResponseContracts/DefaultResponse.cs
+++ Deployr.Web.Contracts/ResponseContracts/DefaultResponse.cs
@@ -4,11 +4,18 @@ using System.Text;
 
 namespace Deployr.Web.Contracts.ResponseContracts
 {
+	public interface IDefaultResponse
+	{
+		bool WasSuccessful { get; set;  }
+		
+		IEnumerable<string> ErrorMessages { get; set; }
+	}
+	
 	/// <summary>
 	/// All reponse contracts should inherit from this.
 	/// Makes it easier for the consumers to check if the web request was successful
 	/// </summary>
-	public abstract class DefaultResponse
+	public abstract class DefaultResponse : IDefaultResponse
 	{
 		public bool WasSuccessful { get; set;  }
 		public IEnumerable<string> ErrorMessages { get; set; }
