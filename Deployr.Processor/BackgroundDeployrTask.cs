0d997e6d852b83b9f8b13057ae901dda8f4c5a7e
Amir <hesamian@uwm.edu>
2020-07-08 01:01:03 -0500

    Redesigned background task to be long-running and added logger

---
+++ Deployr.Processor/BackgroundDeployrTask.cs
@@ -0,0 +1,53 @@
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace Deployr.Processor
+{
+    /// <summary>
+    /// Defines the long-running background task and cancellation token
+    /// </summary>
+    public interface IBackgroundDeployrTask
+    {
+        CancellationTokenSource CancellationToken { get; }
+
+        Task ListenerTask { get; }
+    }
+
+    public class BackgroundDeployrTask : IBackgroundDeployrTask
+    {
+        /// <summary>
+        /// Wait period in Millisecond
+        /// </summary>
+        private const int WaitMillisecond = 5000;
+
+        public CancellationTokenSource CancellationToken { get; }
+
+        public Task ListenerTask { get; }
+
+        public BackgroundDeployrTask(IProcessorTask processorTask, ILogger<BackgroundDeployrTask> logger)
+        {
+            CancellationToken = new CancellationTokenSource();
+
+            var token = CancellationToken.Token;
+
+            ListenerTask = Task.Factory.StartNew(_ =>
+            {
+                while (!CancellationToken.IsCancellationRequested)
+                {
+                    logger.LogTrace("Starting to run listener task");
+
+                    processorTask.Process();
+
+                    logger.LogTrace("Finished running listener task");
+
+                    logger.LogTrace($"Starting to wait {WaitMillisecond}ms before the next run");
+                    
+                    Thread.Sleep(WaitMillisecond);
+
+                    logger.LogTrace($"Finished Waiting {WaitMillisecond}ms for the next run");
+                }
+            }, null, token, TaskCreationOptions.LongRunning, TaskScheduler.Default);
+        }
+    }
+}
